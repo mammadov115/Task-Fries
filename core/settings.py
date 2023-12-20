7eb629f30311f517b8ab946e5645f061d57842b7
mammadov115 <mehmanmehman182@gmail.com>
2023-12-20 22:44:08 +0400

    settings changes

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bntds@b!gh##pbx3r!(nlqfg9x3sbm+3en9ca=sfwkfn2(w7-5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
