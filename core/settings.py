35562a6b8a587bec0bef468b76d612e379b12b0f
mammadov115 <mehmanmehman182@gmail.com>
2023-12-21 00:47:58 +0400

    SS

--- core/settings.py
+++ core/settings.py
@@ -86,10 +86,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'HOST'     : 'app-3465eba8-29fe-4256-926d-21d1e8f4e9db-do-user-13349374-0.c.db.ondigitalocean.com',
+        'PORT'     : '25060',
+        'NAME'     : 'db',
+        'PASSWORD' : 'AVNS_-DXgfpoOikxjKognYfT',
+        'USER' : 'db',
+        # 'sslmode'  : 'require'
     }
 }
 
