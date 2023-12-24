e6eb38caf825e82c93d8091ea620f47404dc96c5
mammadov115 <mehmanmehman182@gmail.com>
2023-12-24 21:19:43 +0400

    added some features

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bntds@b!gh##pbx3r!(nlqfg9x3sbm+3en9ca=sfwkfn2(w7-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
