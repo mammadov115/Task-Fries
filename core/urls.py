abdb5bd410d2127f008542aaf4c3af96fed1adff
mammadov115 <mehmanmehman182@gmail.com>
2023-12-28 16:24:15 +0400

    added some features

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("auth/", include('account.urls'))
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
